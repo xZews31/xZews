76bd7c5b6ab51328793af36cd4297d4433741c42
Glitch (discordjsv12basitaltyapi) <none>
2021-01-13 12:52:33 +0000

    💄🔑 Checkpoint ./komutlar/logo.js:22606388/50 ./komutlar/eğlence.js:22606388/1759

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, args) => {
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
 .setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny Eğlence Komutları <a:Yldz:742698148329291826>")
+.setTitle("<a:zill:798886583737647104>  Destiny Eğlence Komutları <a:zill:798886583737647104> ")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **-tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-fbi** = Bot FBi Gif Atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-token** = Tokenimi Öğrenmek İstemezmisin? <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-düello** = Düello Atarsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-wasted** = Polis tarafından yakalanırsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-atatürk** = Dene ve gör... (1881-1938) <a:developeremoji:749525084586115153>")
+.setDescription("<a:zill:798886583737647104>  **x!tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!fbi** = Bot FBi Gif Atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **x!token** = Tokenimi Öğrenmek İstemezmisin? <a:zill:798886583737647104>  \n <a:yan:750065080741134407> **x!düello** = Düello Atarsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!wasted** = Polis tarafından yakalanırsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!atatürk** = Dene ve gör... (1881-1938) <a:zill:798886583737647104> ")
 .setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
 message.channel.send(eğlence)
 }
