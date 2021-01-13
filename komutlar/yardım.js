8374582974eb29282279f72d0b2bb3d34ccfe510
Glitch (discordjsv12basitaltyapi) <none>
2021-01-13 12:28:38 +0000

    🚲👖 Checkpoint ./komutlar/yardım.js:22606388/553 ./komutlar/logo.js:22606388/550

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,8 +9,8 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("Excellent  v12 Sürümüyle Yeniden Sizlerle.")
 .setTitle("<a:zil:796394115628662826>Excellent Botun'un Yardım Menüsüne Hoşgeldin! <a:zil:796394115628662826>")
  .setTimestamp()
-.setDescription("<a:zill:798886583737647104> **-eğlence** = __Eğlence Komutlarını Görüntülersiniz.__ \n <a:ayarlardestiny:798886582076702741> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <:membericonpng21:798887741642506292> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:Yldz:742698148329291826> **-logo** = __Logo komutlarını görüntülersiniz.__")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:zill:798886583737647104> **x!Eğlence** = __Eğlence Komutlarını Görüntülersiniz.__ \n <:koruma:798883296615858237> **x!Moderasyon** = __Moderasyon Komutlarını Görüntülersiniz.__ \n <:membericonpng21:798887741642506292> **x!Kullanıcı** = __Kullanıcı Komutlarını Görüntülersiniz.__ \n <:Yldz:798889269024391180> **x!Logo** = __Logo Komutlarını Görüntülersiniz.__")
+.setImage("https://cdn.discordapp.com/attachments/798485372392439818/798889938087182376/excellent-sticker-excellent-rounded-isolated-sign-excellent-excellent-sticker-158772142.jpg")
 message.channel.send(yardım)
 }
 
