aca1bd2454877d5da8e0096edb3044998f41d913
Glitch (discordjsv12basitaltyapi) <none>
2021-01-13 13:04:40 +0000

    🌵💁 Checkpoint ./komutlar/eğlence.js:22606388/443 ./komutlar/moderasyon.js:22606388/874

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -6,11 +6,11 @@ exports.run = async (client, message, args) => {
 
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
-.setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:zill:798886583737647104>  Destiny Eğlence Komutları <a:zill:798886583737647104> ")
+.setAuthor("Excellent v12 sürümüyle yeniden sizlerle.")
+.setTitle("<a:zill:798886583737647104>  Excellent Eğlence Komutları <a:zill:798886583737647104> ")
  .setTimestamp()
-.setDescription("<a:zill:798886583737647104>  **x!tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!fbi** = Bot FBi Gif Atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **x!token** = Tokenimi Öğrenmek İstemezmisin? <a:zill:798886583737647104>  \n <a:yan:750065080741134407> **x!düello** = Düello Atarsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!wasted** = Polis tarafından yakalanırsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!atatürk** = Dene ve gör... (1881-1938) <a:zill:798886583737647104> ")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:zill:798886583737647104>  **x!tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!fbi** = Bot FBi Gif Atar. <a:developeremoji:749525084586115153> \n <a:zill:798886583737647104>  **x!token** = Tokenimi Öğrenmek İstemezmisin? <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!düello** = Düello Atarsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!wasted** = Polis tarafından yakalanırsın. <a:zill:798886583737647104>  \n <a:zill:798886583737647104>  **x!atatürk** = Dene ve gör... (1881-1938) <a:zill:798886583737647104> ")
+.setImage("https://cdn.discordapp.com/attachments/798485372392439818/798889938087182376/excellent-sticker-excellent-rounded-isolated-sign-excellent-excellent-sticker-158772142.jpg")
 message.channel.send(eğlence)
 }
 
